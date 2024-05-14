4978cb61ee13ee09cb13dfcc025cd987e92fbdde
minju <deco1202@naver.com>
2024-05-14 22:39:27 +0900

    ~[3-2]

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -7,10 +7,48 @@ public class JpaMain {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
+            //데이터 변경을 해주었으니 다시 persist해서 변경사항을 저장해주어야 하는거 아닌가?
+            // em.persist(member);
+
+            System.out.println("==================");
+
+            tx.commit();
+
+
+//            //비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+//
+//            //영속
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member);
+//            System.out.println("=== AFTER ===");
+//
+//            Member findMember = em.find(Member.class, 101L);
+//
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            System.out.println("findMember.getName() = " + findMember.getName());
+//
+//            tx.commit();
+
+
+
+        }catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }

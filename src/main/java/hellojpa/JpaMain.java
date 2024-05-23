614de68a6ba17e7850f48258be7ea1ff66a53ae7
minju <deco1202@naver.com>
2024-05-23 23:39:23 +0900

    [5-1] 객체지향적이지 못한 코드 예시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,8 +14,22 @@ public class JpaMain {
         tx.begin();
 
         try {
-            tx.commit();
 
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team); //영속 -> 영속상태가 되면 무조건 PK값이 세팅됨. team의 id값
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeamId(team.getId()); //외래키 식별자를 직접 다루고 있음.
+            em.persist(member);
+
+            //찾아온 멤버가 어느 팀 소속인지 알고싶을 때
+            Member findMember = em.find(Member.class, member.getId());
+            Long findTeamId = findMember.getTeamId();
+            Team findTeam = em.find(Team.class, findTeamId);
+
+            tx.commit();
         }catch (Exception e) {
             tx.rollback();
         } finally {
